cc69fe135c2c1dab5e2a155e1445ffdc4ca5b979
ninjagithub-maker <niranjanbabut@gmail.com>
2025-05-30 15:12:54 +0530

    Added Palindrome.java file

---
+++ Palindrome.java
@@ -0,0 +1,12 @@
+public class Palindrome {
+    public static boolean isPalindrome(String s) {
+        s = s.replaceAll("\s+", "".toLowerCase();
+        String reversed = new StringBuilder(s).reverse().toString();
+        return s.equals(reversed);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal, Panama"));
+        System.out.println(isPalindrome("Was it a car or a cat I saw?"));
+    }
+}
